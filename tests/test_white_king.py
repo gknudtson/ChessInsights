855ecd0da68792e078fc8f049cfccfb4b1761e33
gknudtson <gabeknudtson+github@gmail.com>
2024-01-17 19:50:44 -0700

    add testing logic for white king castling

--- tests/test_white_king.py
+++ tests/test_white_king.py
@@ -34,6 +34,7 @@ class TestWhiteKing(unittest.TestCase):
         origin_square = 20
         self.chess_board.add_white_king(origin_square)
         self.chess_board.remove_white_king(4)
+        self.chess_board.update_castling_rights(0)
         self.white_king.movement_manager(origin_square, target_square)
         assert not self.chess_board.is_white_king_on_square(target_square)
 
@@ -50,3 +51,43 @@ class TestWhiteKing(unittest.TestCase):
         self.white_king.movement_manager(self.origin_square, target_square)
         assert not self.chess_board.is_black_piece_on_square(target_square)
         assert self.chess_board.is_white_king_on_square(target_square)
+
+    @parameterized.expand([(2, 0), (6, 7)])
+    def test_valid_castling(self, target_square, rook_square):
+        self.chess_board.add_white_rook(rook_square)
+        self.white_king.movement_manager(self.origin_square, target_square)
+        assert not self.chess_board.is_white_king_on_square(self.origin_square)
+        assert not self.chess_board.is_white_rook_on_square(rook_square)
+
+    def test_invalid_castling_after_king_movement(self):
+        self.white_king.movement_manager(self.origin_square, 5)
+        self.chess_board.change_turn()
+        self.white_king.movement_manager(5, 4)
+        self.chess_board.change_turn()
+        self.white_king.movement_manager(self.origin_square, 6)
+        self.white_king.movement_manager(self.origin_square, 2)
+        assert self.chess_board.is_white_king_on_square(self.origin_square)
+
+    @parameterized.expand([2, 6])
+    def test_invalid_castling_movement_through_attacked_square(self, target_square):
+        self.chess_board.add_black_pawn(self.origin_square + 8)
+        self.white_king.movement_manager(self.origin_square, target_square)
+        assert self.chess_board.is_white_king_on_square(self.origin_square)
+
+    @parameterized.expand([(2, 3), (6, 5)])
+    def test_invalid_castling_white_piece_in_path(self, target_square, white_piece_square):
+        self.chess_board.add_white_bishop(white_piece_square)
+        self.white_king.movement_manager(self.origin_square, target_square)
+        assert self.chess_board.is_white_king_on_square(self.origin_square)
+
+    @parameterized.expand([(2, 3), (6, 5)])
+    def test_invalid_castling_black_piece_in_path(self, target_square, black_piece_square):
+        self.chess_board.add_black_bishop(black_piece_square)
+        self.white_king.movement_manager(self.origin_square, target_square)
+        assert self.chess_board.is_white_king_on_square(self.origin_square)
+
+    @parameterized.expand([2, 6])
+    def test_invalid_castling_king_under_check(self, target_square):
+        self.chess_board.add_black_rook(60)
+        self.white_king.movement_manager(self.origin_square, target_square)
+        assert self.chess_board.is_white_king_on_square(self.origin_square)
