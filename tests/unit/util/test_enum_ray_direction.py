1e85b96837d8eeea2d2c2ee164d08370652e6f58
gknudtson <gabeknudtson+github@gmail.com>
2024-09-22 09:59:12 -0600

    General fixes and small updates

--- tests/unit/util/test_enum_ray_direction.py
+++ tests/unit/util/test_enum_ray_direction.py
@@ -1,15 +1,48 @@
 import unittest
 from parameterized import parameterized
 from chess_insights.util.enum_ray_direction import Direction
+from chess_insights.util.enum_chess_piece_type import ColorChessPiece
 
 
-class TestChessPieceParentClass(unittest.TestCase):
+class TestDirection(unittest.TestCase):
+
     @parameterized.expand(
-        [(0, Direction.SW), (2, Direction.S), (4, Direction.SE), (9, Direction.SW),
-         (10, Direction.S), (11, Direction.SE),
-         (16, Direction.W), (17, Direction.W), (18, Direction._), (19, Direction.E), (20, Direction.E),
-         (25, Direction.NW), (26, Direction.N), (27, Direction.NE), (32, Direction.NW),
-         (33, Direction._), (34, Direction.N), (36, Direction.NE)])
-    def test_ray_direction(self, target_square, expected_result):
-        origin_square = 18
-        assert Direction.from_squares(origin_square, target_square) == expected_result
+        [
+            (18, 10, Direction.S),
+            (18, 26, Direction.N),
+            (18, 27, Direction.NE),
+            (18, 11, Direction.SE),
+            (18, 9, Direction.SW),
+            (18, 25, Direction.NW),
+            (18, 19, Direction.E),
+            (18, 17, Direction.W),
+            (18, 18, Direction.C),
+        ]
+    )
+    def test_ray_direction(self, origin_square, target_square, expected_result):
+        self.assertEqual(Direction.from_squares(origin_square, target_square), expected_result)
+
+    @parameterized.expand(
+        [
+            (ColorChessPiece.WHITE_BISHOP, [Direction.NE, Direction.NW]),
+            (ColorChessPiece.BLACK_BISHOP, [Direction.NE, Direction.NW]),
+            (ColorChessPiece.WHITE_ROOK, [Direction.N, Direction.E]),
+            (ColorChessPiece.BLACK_ROOK, [Direction.N, Direction.E]),
+            (ColorChessPiece.WHITE_QUEEN, [Direction.NE, Direction.NW, Direction.N, Direction.E]),
+            (ColorChessPiece.BLACK_QUEEN, [Direction.NE, Direction.NW, Direction.N, Direction.E]),
+        ]
+    )
+    def test_get_directions(self, piece, expected_directions):
+        self.assertEqual(Direction.get_directions(piece), expected_directions)
+
+    def test_invalid_direction(self):
+        with self.assertRaises(ValueError):
+            Direction.from_squares(18, -5)
+
+    def test_invalid_piece(self):
+        with self.assertRaises(ValueError):
+            Direction.get_directions(ColorChessPiece.WHITE_KNIGHT)
+
+
+if __name__ == '__main__':
+    unittest.main()
