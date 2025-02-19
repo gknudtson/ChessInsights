b11134e4fb50989d09085703728dc9988f155357
gknudtson <gabeknudtson+github@gmail.com>
2025-02-18 17:22:16 -0700

    Fix notation error.

--- src/chess_insights/util/pgn.py
+++ src/chess_insights/util/pgn.py
@@ -17,18 +17,17 @@ def convert_move_pgn(origin_square: int,
                                                                       target_square) == 2:
         pgn += "0-0" if Square(target_square).name.startswith('g') else "0-0-0"
     else:
-        move_notation = f"{piece.fen if piece.fen != 'P' else ''}{'x' if is_capture else ''}{Square(target_square).name}"
+        move_notation = f"{piece.fen.capitalize() if piece.fen.capitalize() != 'P' else ''}{'x' if is_capture else ''}{Square(target_square).name}"
         if piece.piece_type == ChessPieceType.PAWN and is_capture:
             move_notation = f"{Square(origin_square).name[0]}x{Square(target_square).name}"
         pgn += move_notation
-
-    pgn += "+" if is_check else ""
-    pgn += "#" if status == GameStatus.CHECKMATE else ""
 
     if status in {GameStatus.DRAW_50_MOVE, GameStatus.STALEMATE, GameStatus.DRAW_REPETITION,
                   GameStatus.DRAW_INSUFFICIENT_MATERIAL}:
         pgn += " 1/2-1/2"
     elif status == GameStatus.CHECKMATE:
-        pgn += " 1-0" if piece.color == Color.WHITE else " 0-1"
+        pgn += "# 1-0" if piece.color == Color.WHITE else "# 0-1"
+    elif is_check:
+        pgn += "+"
 
     return pgn + " "
